c52e745b47315c6a2cb8af5ff69428a5ffda1170
hyunbikim <152488051+myeonbi@users.noreply.github.com>
2026-04-24 11:47:15 +0900

    Update 1-2.c

--- code/1-2.c
+++ code/1-2.c
@@ -6,9 +6,9 @@ int main() {
     char s2[] = "hi";
     char s3[] = {'h', 'i', '\0'};
 
-    printf("%s\n", s1);
-    printf("%s\n", s2);
-    printf("%s\n", s3);
+    printf("%zu %zu\n", sizeof(s1), strlen(s1));
+    printf("%zu %zu\n", sizeof(s2), strlen(s2));
+    printf("%zu %zu\n", sizeof(s3), strlen(s3));
 
     return 0;
 }
